7dd2d9ac3dac6564973eb884278bad0f96aa37e9
Rami Abdullah <rami.h.abd@gmail.com>
2022-08-28 14:05:37 +0200

    feat(api): add restaurant catalog routes

---
+++ backend/src/modules/restaurants/restaurants.routes.ts
@@ -0,0 +1,56 @@
+import {Router} from 'express';
+import {z} from 'zod';
+
+import {validate} from '../../middleware/validate.js';
+import type {DoorstepRepository} from '../../repositories/repository.js';
+import {asyncHandler} from '../../utils/asyncHandler.js';
+import {notFound} from '../../utils/errors.js';
+
+const paramsSchema = z.object({
+  params: z.object({
+    restaurantId: z.string().min(1)
+  })
+});
+
+export const createRestaurantsRouter = (repository: DoorstepRepository) => {
+  const router = Router();
+
+  router.get(
+    '/',
+    asyncHandler(async (_request, response) => {
+      const restaurants = await repository.listRestaurants();
+      response.json({restaurants});
+    })
+  );
+
+  router.get(
+    '/:restaurantId',
+    validate(paramsSchema),
+    asyncHandler(async (request, response) => {
+      const restaurant = await repository.getRestaurant(request.params.restaurantId);
+      if (!restaurant) {
+        throw notFound('Restaurant not found');
+      }
+
+      const menuItems = await repository.listMenuItems(restaurant.id);
+      response.json({restaurant, menuItems});
+    })
+  );
+
+  router.get(
+    '/:restaurantId/menu',
+    validate(paramsSchema),
+    asyncHandler(async (request, response) => {
+      const restaurant = await repository.getRestaurant(request.params.restaurantId);
+      if (!restaurant) {
+        throw notFound('Restaurant not found');
+      }
+
+      const menuItems = await repository.listMenuItems(restaurant.id);
+      response.json({menuItems});
+    })
+  );
+
+  return router;
+};
+
