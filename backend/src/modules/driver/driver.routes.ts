0c8abb57c5e851c2879af09bcaf834b84e51bcbe
Rami Abdullah <rami.h.abd@gmail.com>
2022-09-13 14:27:48 +0200

    feat(api): add driver routes

---
+++ backend/src/modules/driver/driver.routes.ts
@@ -0,0 +1,42 @@
+import {Router} from 'express';
+import {z} from 'zod';
+
+import {authenticate, authorize} from '../../middleware/auth.js';
+import {validate} from '../../middleware/validate.js';
+import type {DoorstepRepository} from '../../repositories/repository.js';
+import {asyncHandler} from '../../utils/asyncHandler.js';
+
+const statusSchema = z.object({
+  params: z.object({
+    orderId: z.string().min(1)
+  }),
+  body: z.object({
+    status: z.enum(['confirmed', 'preparing', 'ready', 'picked_up', 'delivered', 'cancelled'])
+  })
+});
+
+export const createDriverRouter = (repository: DoorstepRepository) => {
+  const router = Router();
+
+  router.use(authenticate, authorize('driver'));
+
+  router.get(
+    '/orders',
+    asyncHandler(async (request, response) => {
+      const orders = await repository.listOrdersForDriver(request.user?.id ?? '');
+      response.json({orders});
+    })
+  );
+
+  router.patch(
+    '/orders/:orderId/status',
+    validate(statusSchema),
+    asyncHandler(async (request, response) => {
+      const order = await repository.updateOrderStatus(request.params.orderId, request.body.status);
+      response.json({order});
+    })
+  );
+
+  return router;
+};
+
