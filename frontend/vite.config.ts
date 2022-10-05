27cc6501154645221b350cd8222f26fa3f66a65f
Rami Abdullah <rami.h.abd@gmail.com>
2022-10-05 08:56:58 +0200

    feat(web): initialize frontend tooling

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,18 +1,18 @@
-import {defineConfig} from 'vite';
 import react from '@vitejs/plugin-react';
+import {defineConfig} from 'vite';
 
 export default defineConfig({
   plugins: [react()],
   server: {
     port: 5173,
-    strictPort: true,
+    strictPort: true
   },
   preview: {
     port: 4173,
-    strictPort: true,
+    strictPort: true
   },
   build: {
     outDir: 'dist',
-    sourcemap: true,
-  },
+    sourcemap: true
+  }
 });
