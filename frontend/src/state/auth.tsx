89be66d94eba769f8cd3b7d8f6b4df4bb69a4d02
Rami Abdullah <rami.h.abd@gmail.com>
2022-10-21 17:02:47 +0200

    feat(web): add authentication provider

---
+++ frontend/src/state/auth.tsx
@@ -0,0 +1,106 @@
+import {createContext, useCallback, useContext, useEffect, useMemo, useState} from 'react';
+import type {ReactNode} from 'react';
+
+import {api} from '../api/client';
+import type {AuthResponse, User, UserRole} from '../api/types';
+
+interface AuthContextValue {
+  user: User | null;
+  token: string | null;
+  isReady: boolean;
+  login: (input: {email: string; password: string}) => Promise<void>;
+  signup: (input: {name: string; email: string; password: string; role: UserRole}) => Promise<void>;
+  logout: () => void;
+}
+
+const TOKEN_KEY = 'doorstep_token';
+const AuthContext = createContext<AuthContextValue | undefined>(undefined);
+
+export const AuthProvider = ({children}: {children: ReactNode}) => {
+  const [token, setToken] = useState<string | null>(() => localStorage.getItem(TOKEN_KEY));
+  const [user, setUser] = useState<User | null>(null);
+  const [isReady, setIsReady] = useState(false);
+
+  useEffect(() => {
+    let isMounted = true;
+
+    const hydrate = async () => {
+      if (!token) {
+        setIsReady(true);
+        return;
+      }
+
+      try {
+        const response = await api.me(token);
+        if (isMounted) {
+          setUser(response.user);
+        }
+      } catch {
+        localStorage.removeItem(TOKEN_KEY);
+        if (isMounted) {
+          setToken(null);
+          setUser(null);
+        }
+      } finally {
+        if (isMounted) {
+          setIsReady(true);
+        }
+      }
+    };
+
+    void hydrate();
+
+    return () => {
+      isMounted = false;
+    };
+  }, [token]);
+
+  const persistAuth = useCallback((response: AuthResponse) => {
+    localStorage.setItem(TOKEN_KEY, response.token);
+    setToken(response.token);
+    setUser(response.user);
+  }, []);
+
+  const login = useCallback(
+    async (input: {email: string; password: string}) => {
+      persistAuth(await api.login(input));
+    },
+    [persistAuth]
+  );
+
+  const signup = useCallback(
+    async (input: {name: string; email: string; password: string; role: UserRole}) => {
+      persistAuth(await api.signup(input));
+    },
+    [persistAuth]
+  );
+
+  const logout = useCallback(() => {
+    localStorage.removeItem(TOKEN_KEY);
+    setToken(null);
+    setUser(null);
+  }, []);
+
+  const value = useMemo(
+    () => ({
+      user,
+      token,
+      isReady,
+      login,
+      signup,
+      logout
+    }),
+    [isReady, login, logout, signup, token, user]
+  );
+
+  return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
+};
+
+export const useAuth = () => {
+  const value = useContext(AuthContext);
+  if (!value) {
+    throw new Error('useAuth must be used inside AuthProvider');
+  }
+
+  return value;
+};
