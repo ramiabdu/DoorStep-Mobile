ef48482737993a08a6e8382415086501ced29254
Rami Abdullah <rami.h.abd@gmail.com>
2022-10-24 15:50:59 +0200

    feat(web): add cart provider

---
+++ frontend/src/state/cart.tsx
@@ -0,0 +1,83 @@
+import {createContext, useCallback, useContext, useMemo, useState} from 'react';
+import type {ReactNode} from 'react';
+
+import {api} from '../api/client';
+import type {Cart} from '../api/types';
+import {useAuth} from './auth';
+
+interface CartContextValue {
+  cart: Cart | null;
+  isLoading: boolean;
+  refreshCart: () => Promise<void>;
+  addItem: (menuItemId: string, quantity?: number) => Promise<void>;
+  updateItem: (cartItemId: string, quantity: number) => Promise<void>;
+}
+
+const CartContext = createContext<CartContextValue | undefined>(undefined);
+
+export const CartProvider = ({children}: {children: ReactNode}) => {
+  const {token, user} = useAuth();
+  const [cart, setCart] = useState<Cart | null>(null);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const refreshCart = useCallback(async () => {
+    if (!token || user?.role !== 'customer') {
+      setCart(null);
+      return;
+    }
+
+    setIsLoading(true);
+    try {
+      const response = await api.cart(token);
+      setCart(response.cart);
+    } finally {
+      setIsLoading(false);
+    }
+  }, [token, user?.role]);
+
+  const addItem = useCallback(
+    async (menuItemId: string, quantity = 1) => {
+      if (!token) {
+        throw new Error('Please sign in before adding items to the cart.');
+      }
+
+      const response = await api.addCartItem(token, menuItemId, quantity);
+      setCart(response.cart);
+    },
+    [token]
+  );
+
+  const updateItem = useCallback(
+    async (cartItemId: string, quantity: number) => {
+      if (!token) {
+        throw new Error('Please sign in before updating the cart.');
+      }
+
+      const response = await api.updateCartItem(token, cartItemId, quantity);
+      setCart(response.cart);
+    },
+    [token]
+  );
+
+  const value = useMemo(
+    () => ({
+      cart,
+      isLoading,
+      refreshCart,
+      addItem,
+      updateItem
+    }),
+    [addItem, cart, isLoading, refreshCart, updateItem]
+  );
+
+  return <CartContext.Provider value={value}>{children}</CartContext.Provider>;
+};
+
+export const useCart = () => {
+  const value = useContext(CartContext);
+  if (!value) {
+    throw new Error('useCart must be used inside CartProvider');
+  }
+
+  return value;
+};
